eff478aa18cd208f61083616adc6b7ca2f09ed67
yoyojacky <yoyojacky2009@gmail.com>
2021-06-24 14:44:29 +0800

    add dietpi install and uninstall scripts
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- install-dietpi.sh
+++ install-dietpi.sh
@@ -8,20 +8,19 @@ sudo apt-get update && sudo apt-get -y install git python3-pip python3-rpi.gpio
 pip3 install pyserial
 
 
-cd $HOME/deskpi_v1/
 daemonname="deskpi_v1"
 safecutoffpower=/lib/systemd/system/$daemonname-safecutoffpower.service
-installationfolder=$HOME/$daemonname
+installationfolder=/root/$daemonname
 
 # Create service file on system.
 if [ -e $safecutoffpower ]; then
-	sudo rm -f $safecutoffpower
+	rm -f $safecutoffpower
 fi
 
 # adding dtoverlay to enable dwc2 on host mode.
 log_action_msg "Enable dwc2 on Host Mode"
-sudo sed -i '/dtoverlay=dwc2*/d' /boot/config.txt 
-sudo sed -i '$a\dtoverlay=dwc2,dr_mode=host' /boot/config.txt 
+sed -i '/dtoverlay=dwc2*/d' /boot/config.txt 
+sed -i '$a\dtoverlay=dwc2,dr_mode=host' /boot/config.txt 
 if [ $? -eq 0 ]; then
    log_action_msg "dwc2 has been setting up successfully"
 fi
@@ -29,8 +28,8 @@ fi
 # install safe cut off power daemon.
 log_action_msg "DeskPi v1 safe-cut-off-power after system halt daemon"
 cd $installationfolder/drivers/c/ 
-sudo cp -rf $installationfolder/drivers/c/safecutoffpower /usr/bin/safecutoffpower
-sudo chmod 755 /usr/bin/safecutoffpower
+cp -rf $installationfolder/drivers/c/safecutoffpower64 /usr/bin/safecutoffpower64
+chmod 755 /usr/bin/safecutoffpower64
 
 # send signal to MCU before system shuting down.
 echo "[Unit]" > $safecutoffpower
@@ -40,24 +39,24 @@ echo "Before=halt.target shutdown.target poweroff.target" >> $safecutoffpower
 echo "DefaultDependencies=no" >> $safecutoffpower
 echo "[Service]" >> $safecutoffpower
 echo "Type=oneshot" >> $safecutoffpower
-echo "ExecStart=/usr/bin/sudo /usr/bin/safecutoffpower" >> $safecutoffpower
+echo "ExecStart=/usr/bin/safecutoffpower64" >> $safecutoffpower
 echo "RemainAfterExit=yes" >> $safecutoffpower
 echo "TimeoutSec=1" >> $safecutoffpower
 echo "[Install]" >> $safecutoffpower
 echo "WantedBy=halt.target shutdown.target poweroff.target" >> $safecutoffpower
 
 log_action_msg "DeskPi V1 Service configuration finished." 
-sudo chown root:root $safecutoffpower
-sudo chmod 644 $safecutoffpower
+chown root:root $safecutoffpower
+chmod 644 $safecutoffpower
 
 log_action_msg "DeskPi V1 Service Load module." 
-sudo systemctl daemon-reload
-sudo systemctl enable $daemonname-safecutoffpower.service
+systemctl daemon-reload
+systemctl enable $daemonname-safecutoffpower.service
 
 # Finished 
 log_success_msg "DeskPi V1 Driver installation finished successfully." 
 # greetings and require rebooting system to take effect.
 log_action_msg "System will reboot in 5 seconds to take effect." 
-sudo sync
+sync
 sleep 5 
-sudo reboot
+reboot
--- uninstall-dietpi.sh
+++ uninstall-dietpi.sh
@@ -8,13 +8,13 @@ filelocation=/lib/systemd/system/$daemonname
 log_action_msg "Uninstalling DeskPi V1 Driver..."
 sleep 1
 log_action_msg "Remove dtoverlay configure from /boot/config.txt file"
-sudo sed -i '/dtoverlay=dwc2,dr_mode=host/d' /boot/config.txt
+sed -i '/dtoverlay=dwc2,dr_mode=host/d' /boot/config.txt
 log_action_msg "Stop and disable $daemonname"
-sudo systemctl disable $daemonname 2&>/dev/null  
-sudo systemctl stop $daemonname  2&>/dev/null
+systemctl disable $daemonname 2&>/dev/null  
+systemctl stop $daemonname  2&>/dev/null
 log_action_msg "Remove DeskPi V1 Driver..."
-sudo rm -f  $filelocation  2&>/dev/null 
-sudo rm -f /usr/bin/safecutofpower 2&>/dev/null 
+rm -f  $filelocation  2&>/dev/null 
+rm -f /usr/bin/safecutofpower* 2&>/dev/null 
 log_success_msg "Uninstall DeskPi V1 Driver Successfully." 
 log_success_msg "Now You can remove DeskPi V1 Driver folder..." 
 
