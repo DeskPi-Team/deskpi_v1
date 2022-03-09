4b308c78b678122c90a1fee40f59fbd32a4ba3fe
yoyojacky <yoyojacky@users.noreply.github.com>
2022-03-09 16:13:37 +0800

    Create double click safe shutdown script
     double click safe shutdown script

---
+++ drivers/python/double_click_safe_shutdown.py
@@ -0,0 +1,24 @@
+####### Read me first!!! ######################## 
+# Name: double_click_safe_shutdown.py
+# Function:  Read `poweroff` signal from MCU and shutdown Raspberry Pi when the power button has been pressed twice.
+# Require library: pyserial 
+# Install pyserial library by using `pip3 install pyserial` (Python3.x) 
+################################################# 
+#!/usr/bin/python3
+import serial
+import time
+import os
+
+
+ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=10)
+
+while True:
+    if ser.isOpen():
+        data = ser.read(16).decode('utf-8')
+        if 'poweroff' in data:
+            print("System will be shutdown after 3 seconds!")
+            for i in range(1, 4):
+                print(i)
+                time.sleep(1)
+            os.system("sudo sync && sudo init 0")
+        
